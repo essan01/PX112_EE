3eef1824a8e6423fc29917d9101a9353350d7608
Aidan JACQUET <jacquetaidan@gmail.com>
2025-04-18 11:21:29 +0200

    deleted:    basic_display.ino     modified:   include/ctrl_joystick.h 	modified:   src/basic_display.ino 	renamed:    ctrl_joystick.ino -> src/ctrl_joystick.ino

--- include/ctrl_joystick.h
+++ include/ctrl_joystick.h
@@ -1,9 +1,9 @@
 #ifndef CTRL_JOYSTICK_H
 #define CTRL_JOYSTICK_H
 
-void set_joystick_entries(int x, int y, int button) {};
-float joytick_position_x() {};
-float joytick_position_y() {};
-int button_pressed() {};
+void set_joystick_entries(int x, int y, int button);
+float joytick_position_x();
+float joytick_position_y();
+int button_pressed();
 
 #endif CTRL_JOYSTICK_H
