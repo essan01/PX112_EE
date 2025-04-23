20e4dac95ed570d8de16fd25959b33545e804f7c
Aidan JACQUET <jacquetaidan@gmail.com>
2025-04-23 16:12:46 +0200

    modified:   include/Display.h 	modified:   src/Display.ino 	modified:   src/main.ino

--- src/Display.ino
+++ src/Display.ino
@@ -5,28 +5,37 @@ I2C_LCD LCD;
 uint8_t I2C_LCD_ADDRESS = 0x51; // Set the I2C base address for the LCD
 
 int current_page = 0;
-
-char app_list[5][20] = {{"BAD USB"},{"IR"},{"PONG"},{"SETUP"},{"COP MODE"}};
+int _nb_pages;
+char _app_list[10][20]; // Array to hold the app list
 
 void setup_display_infos(uint8_t screen_address, int default_page) {
     I2C_LCD_ADDRESS = screen_address; // Set the I2C address for the LCD
     current_page = default_page; // Set the default page to display
+    
 
     LCD.Init(); // Initialize the LCD
     LCD.CleanAll(WHITE); // Clean the screen with white
     display_page(current_page); // Display the first page
 }
 
+void set_app_list(char app_list[], int nb_pages) {
+    _nb_pages = nb_pages; // Set the number of pages
+    
+    for (int i = 0; i < _nb_pages; i++) {
+        strcpy(_app_list[i], &app_list[i]);
+    }
+}
+
 void display_page(int page){
     LCD.CleanAll(WHITE); // Clean the screen with white
     LCD.FontModeConf(Font_6x8, FM_ANL_AAA, BLACK_BAC);
     LCD.CharGotoXY(0,0);       //Set the start coordinate.
-    LCD.print(app_list[page]);  
+    LCD.print(_app_list[page]);  
 }
   
 void display_next_page(){
     current_page ++;
-    if (current_page > 4){
+    if (current_page > _nb_pages - 1){
         current_page = 0;
         }
     LCD.CleanAll(WHITE);    //Clean the screen with black or white.
@@ -36,7 +45,7 @@ void display_next_page(){
 void display_previous_page(){
     current_page --;
     if (current_page < 0){
-        current_page = 4;
+        current_page = _nb_pages - 1;
         }
     LCD.CleanAll(WHITE);    //Clean the screen with black or white.
     display_page(current_page);
@@ -48,7 +57,7 @@ int get_current_page(){
 
 void display_list(int app_selected) {
     LCD.CleanAll(WHITE); // Clean the screen with white
-    for(int i = 0; i < 5; i++){
+    for(int i = 0; i < _nb_pages; i++){
         if(i == app_selected){
             LCD.FontModeConf(Font_6x8, FM_ANL_AAA, WHITE_BAC);
         }
@@ -56,6 +65,6 @@ void display_list(int app_selected) {
             LCD.FontModeConf(Font_6x8, FM_ANL_AAA, BLACK_BAC);
         }
         LCD.CharGotoXY(0,i*8+4);
-        LCD.print(app_list[i]);
+        LCD.print(_app_list[i]);
     }
 }
--- src/main.ino
+++ src/main.ino
@@ -4,11 +4,16 @@
 #include <Arduino.h>
 #include <Display.h>
 
+#define NB_PAGES 6 // Number of pages available
+char app_list[NB_PAGES][20] = {"MENU","BAD USB","IR","PONG","SETUP","COP MODE"};
+
 int app_selected = 0; // Variable to keep track of the selected app
+int page_selected = 0; // Variable to keep track of the current page
 
 void setup() {
   set_joystick_entries(A0, A1, 7); // Set the joystick pins
   setup_display_infos(0x51, 0); // Set the I2C address and page number
+  set_app_list(&app_list[0][0], NB_PAGES); // Set the app list
   
   Wire.begin();
   Serial.begin(9600);
@@ -20,25 +25,52 @@ void loop() {
   int x = joytick_position_x(); // Get the joystick X position
   int y = joytick_position_y(); // Get the joystick Y position
 
-  if (y > 4.9) { // If joystick is moved to the right
-    app_selected++;
-    if (app_selected > 4) {
-      app_selected = 0; // Loop back to the first app
-    }
-    display_list(app_selected); // Update the display with the new selection
+  switch (page_selected) {
+  case 0: // menu
+    if (y > 4.9) { // If joystick is moved to the right
+      app_selected++;
+      if (app_selected > 4) {
+        app_selected = 0; // Loop back to the first app
+      }
+      display_list(app_selected); // Display the initial list of apps
 
-  } else if (y < 0.1) { // If joystick is moved to the left
-    app_selected--;
-    if (app_selected < 0) {
-      app_selected = 4; // Loop back to the last app
+    } else if (y < 0.1) { // If joystick is moved to the left
+      app_selected--;
+      if (app_selected < 0) {
+        app_selected = 4; // Loop back to the last app
+      }
+      display_list(app_selected); // Display the initial list of apps
     }
-    display_list(app_selected); // Update the display with the new selection
-  }
 
-  if (button_pressed()) { // If the button is pressed
-    display_page(app_selected); // Display the selected app page
-    delay(1000); // Delay for a second to allow the user to see the selection
+    
+
+    if (button_pressed()) { // If the button is pressed
+      page_selected = app_selected; // Set the current page to the selected app
+    }
+    break;
+  
+  case 1:
+    display_page(1);
+    break;
+
+  case 2:
+    display_page(2);
+    break;
+    
+  case 3:
+    display_page(3);
+    break;
+    
+  case 4:
+    display_page(4);
+    break;
+    
+  default:
+    break;
   }
+  
+
+  
 }
 
 
